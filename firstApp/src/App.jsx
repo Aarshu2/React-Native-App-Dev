2bc498452a0be0762c91ef7626b4832c781852a5
Aarsh Bajaj <aarshbajaj22@gmail.com>
2025-04-12 19:29:43 -0400

    V4 PropTypes

--- firstApp/src/App.jsx
+++ firstApp/src/App.jsx
@@ -1,9 +1,13 @@
-import Button from './Button/Button.jsx'
+import Student from './Student.jsx'
 
 function App() {
   return(
     <>
-      <Button/>
+      <Student name="Aarsh" age="30" isStudent={true}/>
+      <Student name="Ben" age={11} isStudent={false}/>
+      <Student name="Brandon" age={27} isStudent={true}/>
+      <Student name="Jayden" age={43} isStudent={false}/>
+      <Student name="Aatman" age={12} isStudent={false}/>
     </>
   );
 }
